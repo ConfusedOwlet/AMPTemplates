89f34330042d766cedfe22054df49c9f59e6fe7d
Rae <17814197+ConfusedOwlet@users.noreply.github.com>
2024-08-03 03:09:51 -0600

    fixing misspellings

--- ark-se-protonscript.sh
+++ ark-se-protonscript.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+#SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+#SCRIPTDIR=$(dirname "$SCRIPT_NAME")
 
-exec 6>display.log
-/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
-XVFB_PID=$!
-while [[ ! -s display.log ]]; do
-  sleep 1
-done
-read -r DPY_NUM < display.log
-rm display.log
+#exec 6>display.log
+#/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
+#XVFB_PID=$!
+#while [[ ! -s display.log ]]; do
+#  sleep 1
+#done
+#read -r DPY_NUM < display.log
+#rm display.log
 
-#mkdir winetricks && cd winetricks
 wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks/winetricks
+chmod +x winetricks
 
 export WINETRICKS=winetricks
 
@@ -22,7 +21,6 @@ pipx upgrade protontricks
 
 protontricks 376030 vcrun2013 vcrun2019 sound=disabled
 
-exec 6>&-
-kill $XVFB_PID
-
+#exec 6>&-
+#kill $XVFB_PID
 exit 0
